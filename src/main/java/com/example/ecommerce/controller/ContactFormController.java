ca6ebe5512519f5480f6b282770163512ed442b8
fanjatiana.pavageau <fanjatiana.pavageau@outlook.com>
2023-05-21 11:01:05 +0200

    feat: create validation message

--- src/main/java/com/example/ecommerce/controller/ContactFormController.java
+++ src/main/java/com/example/ecommerce/controller/ContactFormController.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.Optional;
 
@@ -23,9 +25,15 @@ public class ContactFormController {
                 model.addAttribute("user", user);
                 return "contact-form";
             } else {
-                return "redirect:account";
+                return "redirect:/account";
             }
         }
+
+    @PostMapping("/thanks")
+    public String PostMessage() {
+        return "thanks";
+    }
+
 
 
 }
