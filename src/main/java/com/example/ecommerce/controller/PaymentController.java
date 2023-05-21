712ff2fb500a3d91e83a7ebf6f0b668fa1ba2c09
fanjatiana.pavageau <fanjatiana.pavageau@outlook.com>
2023-05-22 00:01:51 +0200

    feat: create edit delivery address page

--- src/main/java/com/example/ecommerce/controller/PaymentController.java
+++ src/main/java/com/example/ecommerce/controller/PaymentController.java
@@ -1,8 +1,10 @@
 package com.example.ecommerce.controller;
 
 import com.example.ecommerce.entity.Product;
+import com.example.ecommerce.entity.Role;
 import com.example.ecommerce.entity.User;
 import com.example.ecommerce.service.ProductService;
+import com.example.ecommerce.service.RoleService;
 import com.example.ecommerce.service.UserService;
 import jakarta.servlet.http.HttpSession;
 import org.hibernate.annotations.Cascade;
@@ -10,9 +12,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.List;
+import java.util.Optional;
 
 @Controller
 public class PaymentController {
@@ -21,6 +25,9 @@ public class PaymentController {
     ProductService productService;
     @Autowired
     UserService userService;
+
+    @Autowired
+    RoleService roleService;
     @GetMapping("/payment")
     public String showPaymentPage(Model model, HttpSession session) {
         User user = (User) session.getAttribute("user");
@@ -49,5 +56,19 @@ public class PaymentController {
     public String payTheOrder() {
         return "redirect:/payment-validated";
     }
+
+
+    @GetMapping("/edit-delivery-address")
+    public String showEditDeliveryAddressForm(Model model, HttpSession session) {
+        User user = (User) session.getAttribute("user");
+        if (user != null) {
+            model.addAttribute("user", user);
+            return "edit-delivery-address";
+        } else {
+            model.addAttribute("errorMessage", "User not found");
+            return "payment";
+        }
+    }
+
 
 }
